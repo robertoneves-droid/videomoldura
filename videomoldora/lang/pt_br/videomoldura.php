f9133c9c3ba22f43817d091be8a2c154ca540415
Roberto Sarcinelli Neves <roberto.neves@ufes.br>
2026-05-29 20:24:07 -0300

    Delete videomoldora directory

--- videomoldora/lang/pt_br/videomoldura.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$string['modulename'] = 'Vídeo com Moldura';
-$string['modulenameplural'] = 'Vídeos com Moldura';
-$string['pluginname'] = 'Vídeo com Moldura';
-$string['pluginadministration'] = 'Administração do Vídeo com Moldura';
-$string['videourl'] = 'URL do Vídeo (YouTube)';
-$string['videomoldura:addinstance'] = 'Adicionar um novo vídeo com moldura';
--- videomoldora/version.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'mod_videomoldura';
-$plugin->version = 2026052900; // Data atualizada AAAAMMDDXX
-$plugin->requires = 2025041400; // Versão mínima obrigatória para rodar no Moodle 5.0
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.0';
-
