98109d2b34790a46c8c409e9feac1d3f5873f6e2
Roberto Sarcinelli Neves <roberto.neves@ufes.br>
2026-05-29 22:01:01 -0300

    Add files via upload

--- mod_videomoldura/version.php
+++ mod_videomoldura/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videomoldura';
-$plugin->version = 2026052900; // Data atualizada AAAAMMDDXX.
-$plugin->requires = 2025041400; // Versao minima obrigatoria para rodar no Moodle 5.0.
+$plugin->version = 2026052901; // Versão incrementada para o Moodle detectar a atualização.
+$plugin->requires = 2022041900; // Versão mínima obrigatória para rodar a partir do Moodle 4.0.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0';
